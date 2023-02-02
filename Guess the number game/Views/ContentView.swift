1397eebc412213d0a7cc1a13ecd0c9bebf3e3af2
hendrick <sebastinesoacatp@gmail.com>
2023-02-03 00:14:59 +0100

    guess the game.

---
+++ Guess the number game/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Guess the number game
+//
+//  Created by Sebastine Odeh on 02/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AppScaffold()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
